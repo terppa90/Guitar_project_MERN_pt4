efe4cca7855baaa6891a8267a7ce85bac4216bc4
Tero Kaihula <N0465@student.jamk.fi>
2022-02-02 16:05:45 +0200

    Guitar project pt4

---
+++ server/models/postMessage.js
@@ -0,0 +1,24 @@
+import mongoose from 'mongoose';
+
+const postSchema = mongoose.Schema({
+  title: String,
+  message: String,
+  name: String,
+  creator: String,
+  tags: [String], // Array of strings
+  selectedFile: String,
+  likes: {
+    //object
+    type: [String],
+    default: [],
+  },
+  createdAt: {
+    //object
+    type: Date,
+    default: new Date(),
+  },
+});
+
+var PostMessage = mongoose.model('PostMessage', postSchema);
+
+export default PostMessage;
---
+++ server/routes/posts.js
@@ -0,0 +1,24 @@
+import express from 'express';
+
+import {
+  getPosts,
+  getPostsBySearch,
+  getPost,
+  createPost,
+  updatePost,
+  likePost,
+  deletePost,
+} from '../controllers/posts.js';
+
+const router = express.Router();
+import auth from '../middleware/auth.js';
+
+router.get('/search', getPostsBySearch);
+router.get('/', getPosts);
+router.post('/', auth, createPost);
+router.get('/:id', getPost);
+router.patch('/:id', auth, updatePost);
+router.delete('/:id', auth, deletePost);
+router.patch('/:id/likePost', auth, likePost);
+
+export default router;
